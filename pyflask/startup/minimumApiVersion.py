e9d4a28df36226a2959eb84287d23ffb66256b34
slugb0t <wheresdorian@gmail.com>
2023-02-24 17:20:40 -0800

    refactor: ♻️ re-release

--- pyflask/startup/minimumApiVersion.py
+++ pyflask/startup/minimumApiVersion.py
@@ -2,6 +2,6 @@ import os
 
 def get_api_version():
     """
-    Returns the version of the API
+    Returns the version of the API (test)
     """
     return {'version': os.getenv('API_VERSION', "10.0.5")}
